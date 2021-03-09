64bf4abd0b4514a10aecada455547ac2669afdb0
Ankur Patel <ankurpatel544@gmail.com>
2021-03-08 20:29:24 -0500

    Initial commit of the JS udemy course

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,39 @@
+/* Values and Variables */
+const country = "United States of America";
+const continent = "North America";
+let population = 382.2 + " million";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+/* Data Types */
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+
+/* let, const, and var */
+
+language = "Gujarati";
+// let country = "USA"; // Uncaught SyntaxError: Identifier 'country' has already been declared
+// let continent = "NA"; //Uncaught SyntaxError: Identifier 'continent' has already been declared
+
+console.log(language);
+console.log(country);
+console.log(continent);
+
+
+/* Basic Operators */
+/* Strings and Template Literals */
+/* Taking Decisions: if / else Statements */
+/* Type Conversion and Coersion */
+/* Equality Operators: == vs. === */
+/* Logical Operators */
+/* The switch Statement */
+/* The Conditional (Ternary) Operator */
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,64 @@
+/* LECTURE 1 - JS Fundamentals Part 1
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Ankur');
+console.log('23');
+
+let firstName = "Ankur";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Ankur');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 2021;
+
+console.log(typeof year);
+
+console.log(typeof null);
+
+
+//DEFAULT- use const to start and when you need a mutatable variable then use let.
+
+//When to use Let variable - when you are  planning on changing the value of the variable.
+let age = 30;
+age = 21;
+
+//When to use Const Variable - the variable is not going to be changed and need to remain the same throughout.
+const birthYear = 1991;
+// birthYear = 1990; // not allowed because the variable birthYear is a Const variable
+
+// when using Const we need to use an intial variable.
+// const job;
+
+//Never use var because its the legacy way and doesnt really allow strict rules against changing the value.
+var job = "programmer";
+job = "teacher";
+
+
+//
+lastName = "Patel";
+console.log(lastName);
+
+*/
+
+/**************Basic Operators**********/
+
