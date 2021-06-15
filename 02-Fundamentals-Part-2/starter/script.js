936c5272193b11512c3cdcb24adc29cc79f0e850
Ankur Patel <ankurpatel544@gmail.com>
2021-06-15 19:59:09 -0400

    adding comments on strict usage

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+//using strict will throw error messages when the code has an error.
 let hasLicense = false;
 const passTest = true;
 
@@ -9,4 +9,5 @@ if(hasLicense) console.log("I can drive!");
 
 // const private = 1234;
 // const interface = 'audio';
+
 
