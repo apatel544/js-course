867d3a169e79a81d539583ff182c457729207c8f
Ankur Patel <ankurpatel544@gmail.com>
2021-07-25 16:22:57 -0400

    Relearning functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 //using strict will throw error messages when the code has an error.
+
 // let hasLicense = false;
 // const passTest = true;
 
@@ -7,8 +8,10 @@
 
 // if(hasLicense) console.log("I can drive!");
 
+//This throws an error because it is not a valid variable name.
 // const private = 1234;
 // const interface = 'audio';
+
 
 /*
 function logger() {
@@ -17,7 +20,9 @@ function logger() {
 }
 //calling, running, or invoking the function
 logger();
+*/
 
+/*
 //functions can reuse a piece of code but can also take in data.(parameters)
 
 function fruitProcessor(apples, oranges) {
@@ -31,6 +36,8 @@ console.log(appleJuice);
 const appleOrangeJuice = fruitProcessor(2,3);
 console.log(appleOrangeJuice);
 
+
+
 // Function Declaration
 function calcAge1(birthYear) {
     return 2021 - birthYear;
@@ -43,14 +50,13 @@ const calcAge2 = function (birthYear) {
 }
 const age2 = calcAge2(1996);
 console.log(age1, age2);
-*/
-
 
 // Arrow Functions
 
 const calcAge3 = birthYear => 2021 - birthYear;
 const age3 = calcAge3(1996);
 console.log(age3);
+
 
 const yearsToRetirement = (birthYear, firstName) => {
     const age = 2021 - birthYear;
@@ -61,3 +67,19 @@ const yearsToRetirement = (birthYear, firstName) => {
 
 console.log(yearsToRetirement(1996, 'Ankur'));
 
+
+
+function fruitCutter(fruit)  {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = fruitCutter(apples);
+    const orangePieces = fruitCutter(oranges);
+ 
+    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+    return juice;
+}
+ 
+console.log(fruitProcessor(2, 3));
+*/
